3bf391728e91e83b56d704308dc6f41f999d46bb
sha-gupta14 <ershashank14@gmail.com>
2018-04-23 22:30:16 +0530

    testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const getPath = path.join(__dirname, '..');
-const port = 8080;
+const port = process.env.PORT || 2000;
 
 app.use(express.static(getPath));
 
@@ -10,6 +10,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(getPath, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log("Express server is running...");
+// app.listen(port, () => {
+//     console.log("Express server is running...");
+// });
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
 });
